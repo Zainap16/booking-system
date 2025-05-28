e6b294ca8b81c73ce2725760805be1f986b645b2
Zainap16 <zvanblerck1@gmail.com>
2025-05-28 17:09:53 +0200

    updates H86 and 6PM

--- server.js
+++ server.js
@@ -32,7 +32,7 @@ const MAX_SEATS = 43;
 const ROOMS = [
   "F52", "F53", "F54", "F55", "F56", "F57", "F58", "F59", "F60", "F61", "F62", "F63",
   "G64", "G65", "G66", "G67", "G68", "G69", "G70", "G71", "G72", "G73", "G74", "G75", "G76", "G77", "G78",
-  "H79", "H80", "H81", "H82", "H83", "H84", "H85", "H87", "H88", "H89", "H90", "H91", "H92", "H93"
+  "H79", "H80", "H81", "H82", "H83", "H84", "H85","H86", "H87", "H88", "H89", "H90", "H91", "H92", "H93"
 ];
 
 // ✅ Email transporter
@@ -184,9 +184,9 @@ app.delete("/cancel", async (req, res) => {
 
     if (bookingDate.getTime() === tomorrow.getTime()) {
       const noonToday = new Date(today);
-      noonToday.setHours(12, 0, 0, 0);
+      noonToday.setHours(18, 0, 0, 0);
       if (now >= noonToday) {
-        return res.status(400).send("Cancellation not allowed after 12 PM the day before the booking.");
+        return res.status(400).send("Cancellation not allowed after 6 PM the day before the booking.");
       }
     }
 
